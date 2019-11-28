1a61ae4de325c9c281b4fc76a5b4b87cac2e76b1
AnnyHuynh <anny_huynh32@yahoo.com>
2019-11-28 14:10:40 -0800

    updated

---
+++ insertionSortList.js
@@ -0,0 +1,10 @@
+//https://leetcode.com/problems/insertion-sort-list/
+//Sort a linked list using insertion sort.
+//Input: 4->2->1->3
+//Output: 1->2->3->4
+function insertionSortList(head){
+  if(!head || !head.val){
+    return head;
+  }
+  
+}
---
+++ sortMerge.js
@@ -0,0 +1,8 @@
+// Cracking the Coding Interview: Question 10.1 page 149
+//you are given two sorted arrays, A and B, where A has a large enough buffer at the end to hold B. Write a method to merge B into A in sorted order. 
+function sortMerge(A, B){
+  let len1 = A.length;
+  let len2 = B.length;
+ 
+}
+console.log(sortMerge([3, 8], [2, 6, 7]));
