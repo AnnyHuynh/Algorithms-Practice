9963bbf5d23f045a27a9c91e696f391c5a305c80
AnnyHuynh <anny_huynh32@yahoo.com>
2019-11-28 14:21:39 -0800

    updated

--- mergeSortedArr.js
+++ mergeSortedArr.js
@@ -1,4 +1,4 @@
-//you are given two sorted arrays, A and B. Write a method to merge A and B in a sorted order. 
+//you are given two sorted arrays, A and B. Write a method to merge A and B in sorted order. 
 // input: A = [3, 8]; B = [2, 6, 7]
 // Output: C = [2, 3, 6, 7, 8];
 function sortMerge(A, B){
@@ -8,7 +8,7 @@ function sortMerge(A, B){
   let len1 = A.length;
   let len2 = B.length;
   while(i < len1 && j < len2){
-    if(A[i] < B[j]){
+    if(A[i] <= B[j]){
       C.push(A[i]);
       i++;
     }else{
@@ -28,4 +28,4 @@ function sortMerge(A, B){
   }
   return C;
 }
-console.log(sortMerge([3, 8], [2, 6, 7]));
+console.log(sortMerge([3, 6, 8], [2, 6, 7]));
