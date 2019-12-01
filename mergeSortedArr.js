01f2625f441859973b24a5a9aa4b626d21dbe905
AnnyHuynh <anny_huynh32@yahoo.com>
2019-11-30 17:38:01 -0800

    updated

--- mergeSortedArr.js
+++ mergeSortedArr.js
@@ -1,13 +1,17 @@
 //you are given two sorted arrays, A and B. Write a method to merge A and B in sorted order. 
 // input: A = [3, 8]; B = [2, 6, 7]
 // Output: C = [2, 3, 6, 7, 8];
+//solution below Time: O(n+m), Space: O(n+m)
 function sortMerge(A, B){
+  // given a variable empty array C to hold result
   let C = [];
   let i = 0;
   let j = 0;
   let len1 = A.length;
   let len2 = B.length;
   while(i < len1 && j < len2){
+    //traversal through A and B, compare each number in A to each number in B. 
+    // if int of A is less than or equal to int of B, then push int of A into C, and increment i in A. Otherwise, push int of B into C, and increment j in B.
     if(A[i] <= B[j]){
       C.push(A[i]);
       i++;
@@ -16,6 +20,7 @@ function sortMerge(A, B){
       j++;
     }
   }
+  //if len1 and len2 are not equal, then check if there's any element left in A or B, and push the rest of elements into C. 
   while(i < len1 || j < len2){
     if(A[i]){
       C.push(A[i]);
@@ -28,4 +33,4 @@ function sortMerge(A, B){
   }
   return C;
 }
-console.log(sortMerge([3, 6, 8], [2, 6, 7]));
+console.log(sortMerge([3, 6, 8], [2, 6, 7])); //C = [2, 3, 6, 6, 7, 8];
--- sortMerge.js
+++ sortMerge.js
@@ -1,8 +1,31 @@
 // Cracking the Coding Interview: Question 10.1 page 149
 //you are given two sorted arrays, A and B, where A has a large enough buffer at the end to hold B. Write a method to merge B into A in sorted order. 
+//solution below has O(n+m) in time complexity and O(1) space complexity
 function sortMerge(A, B){
-  let len1 = A.length;
-  let len2 = B.length;
- 
+  //k is the index the last int of buffer Array A 
+  let k = A.length + B.length - 1;
+  // i is the index of the last int in array A
+  let i = A.length - 1;
+  // j is the index of the last int in array B
+  let j = B.length - 1;
+  // while there is still number in array B
+  while(j >= 0){
+    //if there's also number in array A, and last int of A is larger than last int of B
+    if(i >= 0 && A[i] > B[j]){
+      //move last int of A to the last index position in buffer A
+      A[k] = A[i];
+      // decrease last int of A by 1
+      i--;
+    }else{
+      //if last int of A is smaller than last int of B, then set last int of buffer A equal to last int of B
+      A[k] = B[j];
+      //decrease last int of B by 1
+      j--;
+    }
+    //decrease last int of buffer A by 1
+    k--;
+  }
+  //return array A
+  return A;
 }
-console.log(sortMerge([3, 8], [2, 6, 7]));
+console.log(sortMerge([3, 8, 9], [-1, 0, 6, 9])); // Output: A = [-1, 0, 3, 6, 8, 9, 9]
