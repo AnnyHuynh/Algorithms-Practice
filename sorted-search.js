305cdb7076ecd1b03c36ed22c2e9ebac10b6b769
AnnyHuynh <anny_huynh32@yahoo.com>
2019-12-08 18:27:42 -0800

    updated

--- sorted-search.js
+++ sorted-search.js
@@ -1,4 +1,5 @@
 //Cracking the coding interview 10.4 page 150
+//Leetcode: https://code.dennyzhang.com/search-in-a-sorted-array-of-unknown-size
 //You are given an array-like data structure Listy which lacks a size method. It does, however have an element At(i) method that returns the element at index i in O(1) time. If it is beyond the bounds of the data structure, it returns -1. (For this reason, the data structure only supports positive integers.) Given a Listy which contains sorted, positive integers, find the index at which an element x occurs. If x occurs multiple times, you may return any index.
 
  
