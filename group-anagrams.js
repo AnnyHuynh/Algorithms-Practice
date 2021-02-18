51cce2b3d9023d64b2899ee6346817a1de671119
AnnyHuynh <anny_huynh32@yahoo.com>
2021-02-17 20:20:25 -0800

    Destructure if else statement

--- group-anagrams.js
+++ group-anagrams.js
@@ -8,3 +8,26 @@
 // Input: strs = ["eat","tea","tan","ate","nat","bat"]
 // Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
 
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function(strs) {
+		const obj = {};
+		const len = strs.length;
+
+		if(len <= 1) return [strs];
+		
+		for (let i = 0; i < len; i++){
+			let val = strs[i].split('').sort();
+
+			obj[val] = obj[val] ? [...obj[val], strs[i]] : [strs[i]];
+		
+		}
+
+		return Object.values(obj);
+};
+
+console.log(groupAnagrams(["eat","tea","tan","ate","nat","bat"]))
+console.log(groupAnagrams([""]))
+console.log(groupAnagrams(["a"]))
